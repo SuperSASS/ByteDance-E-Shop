a6b81f3e75096723a4ba03640fccb5a09917b7c2
Super SASS…… <mrniconiconi@gmail.com>
2025-11-23 18:10:20 +0800

    feat(shop-web): 实现主题(Theme)支持

--- apps/shop-web/src/main.tsx
+++ apps/shop-web/src/main.tsx
@@ -4,9 +4,12 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
 import './index.css'
 import './i18n'
+import { ThemeProvider } from './hooks/useTheme'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </StrictMode>
 )
